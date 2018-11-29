845964ccceed46ec9c972cc877eb49dd2e81ff0a
Joseph Despain <jspankin5@gmail.com>
2018-11-28 23:07:48 -0800

    Initial commit

---
+++ VulkanFun/VulkanFun.cpp
@@ -0,0 +1,11 @@
+// VulkanFun.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "MainApp.h"
+
+int main() {
+	MainApp app;
+	app.Run();
+	return 0;
+}
